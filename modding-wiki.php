73e0f68b62c4158564a5fcd7a7f8d92c2dec50cc
Jordan <jordsta95@users.noreply.github.com>
2016-01-07 10:42:10 +0000

    Nicer way to get to the modding tutorial wiki

---
+++ modding-wiki.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Temporal Reality - Modded Minecraft Community | Contact</title>   
+
+<meta name="name" content="Temporal Reality - Modded Minecraft Community" /> 
+<meta name="description" content="Temporal Reality is a gaming community, centred around modded Minecraft. Specialized in making high quality mod packs for a custom launcher.">
+<meta name="keywords" content="minecraft, mods, modded minecraft, forums, forum, launcher, community, mod packs, modpacks">
+<meta name="author" content="Jordan 'jordsta95' Hoyland">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<?php include_once("phpFiles/analytics.php") ?>
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+<?php include 'phpFiles/header.php' ?>
+
+			<!-- Main -->
+<div style="padding-left:5%; padding-right:5%;">
+								<!-- Content -->
+									<section id="content">										
+										<h3>Modding Wiki</h3>
+										<p>The following wiki aims to help people, who want to learn how to mod, who are new to modding minecraft</p>
+<strong>The tutorials here may assume you understand the basics of Java</strong>
+</section>
+<iframe width="100%" height="900px" src="http://modwiki.temporal-reality.com/mw/index.php/Main_Page" style="padding-top:1%;">Your browser doesn't support iframes. It would be a good idea to update it.</iframe>
+</div>
+			<!-- Footer -->
+				<section id="six" class="wrapper style2 special fade">
+					<div class="container">
+						<div class="section group">
+						<div class="col span_1_of_2">
+	<h4>Twitter</h4><br>
+<a class="twitter-timeline" href="https://twitter.com/TemporalReality" data-widget-id="615087875902828545">Tweets by @TemporalReality</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	</div>
+	<div class="col span_1_of_2">
+	<h4>News</h4><div style="text-align:left;">
+<script src="http://feeds.feedburner.com/tr-news?format=sigpro" type="text/javascript" ></script>
+<noscript><p>Subscribe to RSS headline updates from: <a href="http://feeds.feedburner.com/tr-news">
+Powered</a> by FeedBurner</p> </noscript>
+</div>
+	</div>
+</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+<?php include 'phpFiles/footer.php' ?>
+		</div>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
